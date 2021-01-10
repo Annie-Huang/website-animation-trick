fcde01d0677af782de9d227ef65fe1103aa32488
Annie Huang <annie.hn@gmail.com>
2021-01-10 16:05:44 +1100

    Animation: add "-=1" means 1 seconds before the previous tween ends  (overlap with the previous tween)

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ tl.to('.text', {y: "0%", duration: 1, stagger: 0.25});
 
 // use timeline it will chain things together the next one happens when the first one finishes.
 tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
-tl.to('.intro', {y: '-100%', duration: 1});
+
+// -=1 means 1 seconds before the previous tween ends  (overlap with the previous tween)
+// So "duration: 1" and '-=1' makes sure the .intro change will finish at the same time as the .slider change.
+tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
