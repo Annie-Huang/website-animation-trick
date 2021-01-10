75fdfc790a1d9a123c1417d2cf43fb75af961a43
Annie Huang <annie.hn@gmail.com>
2021-01-10 15:47:58 +1100

    Animation: stagger: 0.25 controls that it pops up one by one for all element that contains .text for the given timeframe

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 // Create a timeline from gsap so we can apply to different item.
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
-tl.to('.text', {y: "0%", duration: 1})
+tl.to('.text', {y: "0%", duration: 1, stagger: 0.25})
