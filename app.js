25120b1140e03390f1b42b7bab935a3f94bec4dc
Annie Huang <annie.hn@gmail.com>
2021-01-10 15:54:44 +1100

    Animation: Move up .intro after moving up .slider

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Create a timeline from gsap so we can apply to different item.
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
-tl.to('.text', {y: "0%", duration: 1, stagger: 0.25})
+tl.to('.text', {y: "0%", duration: 1, stagger: 0.25});
 
 // use timeline it will chain things together the next one happens when the first one finishes.
-tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5})
+tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
+tl.to('.intro', {y: '-100%', duration: 1});
